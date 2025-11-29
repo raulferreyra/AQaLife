3700595d3b9296f7f20144cc270b972c5c547123
Raúl Ferreyra <raulrferreyrav@gmail.com>
2025-11-29 12:23:08 -0500

    Add LoginFragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation(libs.androidx.annotation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
